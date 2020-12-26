0150fb3e4c004499b669b9b8680898d6532c585c
Boris Serebrov <serebrov@gmail.com>
2020-12-26 13:39:53 +0200

    Add helper script to pull upstream

---
+++ etc/pull-upstream.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# git tag custom/$(date "+%Y-%m-%d-%H-%M")-before
+# git push origin --tags
+
+# git checkout master
+git fetch zsa
+# git pull upstream master
+
+# git push origin master
+# git checkout custom
+
+# git merge master
+
+# git tag custom/$(date "+%Y-%m-%d-%H-%M")-after
+
+# git push origin HEAD
+# git push origin --tags
